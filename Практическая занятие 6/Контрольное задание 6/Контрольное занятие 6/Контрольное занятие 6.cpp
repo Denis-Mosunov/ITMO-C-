b2151207cdf8fed892ceddc444259b4117b4f2e8
Denis-Mosunov <dnsmosunov@gmail.com>
2025-03-31 14:58:07 +0300

    Add files via upload

---
+++ Практическая занятие 6/Контрольное задание 6/Контрольное занятие 6/Контрольное занятие 6.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    ofstream outFile; // Объект для записи в файл
+    string poemLine;
+    string filename;
+
+    // Запрос имени файла
+    cout << "Введите имя файла для сохранения стихотворения: ";
+    getline(cin, filename);
+
+    // Открытие файла
+    outFile.open(filename);
+
+    if (!outFile.is_open()) {
+        cerr << "Ошибка: не удалось открыть файл!" << endl;
+        return 1;
+    }
+
+    cout << "Введите стихотворение (для завершения введите пустую строку):\n";
+
+    // Чтение ввода построчно
+    while (true) {
+        getline(cin, poemLine);
+        if (poemLine.empty()) {
+            break;
+        }
+        outFile << poemLine << endl; // Запись строки в файл
+    }
+
+    outFile.close(); // Закрытие файла
+    cout << "Стихотворение успешно сохранено в файл \"" << filename << "\"" << endl;
+
+    return 0;
+}
