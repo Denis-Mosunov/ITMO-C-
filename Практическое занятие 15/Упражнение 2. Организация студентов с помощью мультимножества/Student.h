e9669ecccdd019ae22818e63abca6da79fa43769
Denis-Mosunov <dnsmosunov@gmail.com>
2025-04-15 22:45:30 +0300

    Create Student.h

---
+++ Практическое занятие 15/Упражнение 2. Организация студентов с помощью мультимножества/Student.h
@@ -0,0 +1,36 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+#include "IdCard.h"
+
+class Student {
+private:
+    std::string name;
+    std::string last_name;
+    IdCard* idCard;
+
+public:
+    Student(const std::string& n, const std::string& ln, IdCard* id = nullptr)
+        : name(n), last_name(ln), idCard(id) {}
+    
+    void display() const {
+        std::cout << last_name << " " << name;
+        if(idCard) std::cout << ", ID: " << idCard->getNumber();
+        std::cout << std::endl;
+    }
+
+    friend bool operator< (const Student&, const Student&);
+    friend bool operator> (const Student&, const Student&);
+    friend bool operator== (const Student&, const Student&);
+    friend bool operator!= (const Student&, const Student&);
+};
+
+class compareStudent {
+public:
+    bool operator() (const Student* ptrSt1, const Student* ptrSt2) const {
+        return *ptrSt1 < *ptrSt2;
+    }
+};
+
+#endif
