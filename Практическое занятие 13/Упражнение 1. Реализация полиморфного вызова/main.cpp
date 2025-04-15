9b72807a25bc433384f969b6ca1bbbada9705e7b
Denis-Mosunov <dnsmosunov@gmail.com>
2025-04-15 22:21:22 +0300

    Create main.cpp

---
+++ Практическое занятие 13/Упражнение 1. Реализация полиморфного вызова/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <windows.h>
+#include "item.h"
+#include "paperbook.h"
+#include "audiobook.h"
+
+int main() {
+    SetConsoleOutputCP(1251);
+    Item* pubarr[100];
+    
+    int n = 0;
+    char choice;
+    do {
+        std::cout << "\nВводить данные для книги или звукового файла (b/a)? ";
+        std::cin >> choice;
+        
+        if (choice == 'b') {
+            pubarr[n] = new Paperbook;
+        } else {
+            pubarr[n] = new AudioBook;
+        }
+        
+        pubarr[n++]->getdata();
+        
+        std::cout << "Продолжать (y/n)? ";
+        std::cin >> choice;
+    } while (choice == 'y');
+    
+    for (int j = 0; j < n; j++) {
+        pubarr[j]->putdata();
+        std::cout << std::endl;
+        delete pubarr[j]; // Освобождаем память
+    }
+    
+    return 0;
+}
